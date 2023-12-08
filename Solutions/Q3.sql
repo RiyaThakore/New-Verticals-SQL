01b0c742237204c71dd1deefc32cdb4163b37b8b
Riya Thakore <30748253+RiyaThakore@users.noreply.github.com>
2023-12-07 16:55:26 -0800

    Update Q3.sql

--- Solutions/Q3.sql
+++ Solutions/Q3.sql
@@ -1,1 +1,6 @@
 
+
+SELECT
+COUNT(DISTINCT CASE WHEN account_owner = 'Kevin' THEN store_id END) AS kevin_owned_stores
+, COUNT(DISTINCT CASE WHEN account_owner = 'Carla' THEN store_id END) AS carla_owned_stores
+FROM BusinessData b JOIN AccountOwner_Business_Mapping a ON b.business_id = a.business_id
