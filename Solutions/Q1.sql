940a039d6c9394ebfaba19c5c66cfdab44adee4c
Riya Thakore <30748253+RiyaThakore@users.noreply.github.com>
2023-12-07 17:04:31 -0800

    Update Q1.sql

--- Solutions/Q1.sql
+++ Solutions/Q1.sql
@@ -3,14 +3,5 @@ COUNT(DISTINCT Delivery_id) AS Delivery_cnt
 , SUM(order_value) AS Revenue
 FROM DeliveryData
 WHERE 1=1
-AND MONTH(date) = 8 --August month
+AND strftime('%m', date) = '08' --August month
 AND store_id = 123 --Store ID = 123
-
-
-
-
-
-
-
-
-
