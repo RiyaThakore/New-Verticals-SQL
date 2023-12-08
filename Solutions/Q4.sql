78d6f91a97893b887405d5a2bfff482bbc89a3eb
Riya Thakore <30748253+RiyaThakore@users.noreply.github.com>
2023-12-07 17:26:07 -0800

    Update Q4.sql

--- Solutions/Q4.sql
+++ Solutions/Q4.sql
@@ -1,7 +1,11 @@
--- Monthly sales for stores managed by Kevin and Carla with cumulative total
+
+-- Monthly sales for stores managed by Kevin and Carla (both) with cumulative total
+-- Grouping by stores as well to give store-wise sales
+-- For all stores managed by Kevin and Carla and not specific store wise values, can certainly do that by introducing another case-when in the CTE
+
 WITH MonthlySales AS (
     SELECT
-        DATE_FORMAT(date, '%Y-%m') AS month,
+        strftime('%m-%Y', date) AS month_year,
         store_id,
         SUM(order_value) AS monthly_sales
     FROM
@@ -9,20 +13,20 @@ WITH MonthlySales AS (
     WHERE
         store_id IN (
             SELECT DISTINCT store_id
-            FROM AccountOwner_Business_Mapping
+            FROM BusinessData b JOIN AccountOwner_Business_Mapping a ON b.business_id = a.business_id
             WHERE account_owner IN ('Kevin', 'Carla')
         )
     GROUP BY
-        month, store_id
+        month_year, store_id
     ORDER BY
-        month, store_id
+        month_year, store_id
 )
 SELECT
-    month,
+    month_year,
     store_id,
     monthly_sales,
-    SUM(monthly_sales) OVER (PARTITION BY store_id ORDER BY month) AS cumulative_sales
+    SUM(monthly_sales) OVER (PARTITION BY store_id ORDER BY month_year) AS cumulative_sales
 FROM
     MonthlySales
 ORDER BY
-    store_id, month;
+    store_id, month_year;
